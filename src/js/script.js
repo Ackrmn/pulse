706538ffc62ad08d8138f5adf19e8f953605e7f1
Ackrmn <alexandranisimow@yandex.ru>
2019-10-05 10:57:35 +0300

    slider omg

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/arrow_left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/slider/arrow_right.png"></button>'
+    });
+  });
