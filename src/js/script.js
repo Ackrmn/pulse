a776454b79305f6f619d953c4be32f7caf4b7e43
Ackrmn <alexandranisimow@yandex.ru>
2019-10-12 11:52:56 +0300

    unworked script)

--- src/js/script.js
+++ src/js/script.js
@@ -4,4 +4,10 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/arrow_left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="img/slider/arrow_right.png"></button>'
     });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.col-md-10.offset-md-1').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
   });
